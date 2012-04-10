c324f190314c23ccb8991bb2c01c26747dc8fb2f
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-04-10 18:56:13 -0300

    Adicionado o plugin CakePtbr.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,29 +1,8 @@
 <?php
 /**
- * This file is loaded automatically by the app/webroot/index.php file after core.php
+ * Setup a 'default' cache configuration for use in the application.
  *
- * This file should load/create any application wide configuration settings, such as
- * Caching, Logging, loading additional configuration files.
- *
- * You should also use this file to include any files that provide global functions/constants
- * that your application uses.
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Config
- * @since         CakePHP(tm) v 0.10.8.2117
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-
-// Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
 
 /**
@@ -63,3 +42,4 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
+CakePlugin::load('CakePtbr', array('bootstrap' => true));
